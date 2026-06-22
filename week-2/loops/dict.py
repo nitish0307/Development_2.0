c1d102fa500b7aa9ee1073a1fc29bdecca59c04d
Nitish Mishra <105452945+nitish0307@users.noreply.github.com>
2026-06-22 15:12:17 +0000

    Add week 2

---
+++ week-2/loops/dict.py
@@ -0,0 +1,10 @@
+marks = {
+    "Maths": "99",
+    "Science": "96",
+    "English": "98"
+}
+
+# print(marks)
+
+for i in marks:
+    print(i, marks[i], sep=":")
---
+++ week-2/loops/list.py
@@ -0,0 +1,9 @@
+name = ["Nitish", "Ritesh", "Nibha", "Nagendra"]
+
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+
+for i in range(len(name)):
+    print(i+1, name[i])
